3986045ac2c50f9ffa87ca6cba7d31eff881b0fc
jackpopp <jack.popp@hotmail.com>
2015-06-03 07:58:14 +0100

    Initial commit with some sections completed.

---
+++ main.js
@@ -0,0 +1,67 @@
+var HEIGHT = 600;
+var WIDTH = 800;
+var game = new Phaser.Game(800, 600, Phaser.AUTO, null, { preload: preload, create: create, update: update });
+var background;
+var platforms;
+var platformData = [
+    {x: 0, y: 570, width: 800},
+    {x: 50, y: 200, width: 250},
+    {x: 325, y: 400, width: 180},
+    {x: 500, y: 200, width: 250}
+]
+var player;
+
+function preload()
+{
+    game.load.image('background', 'assets/images/background.png');
+    game.load.image('player', 'assets/images/player.png');
+    game.load.image('platform', 'assets/images/platform.png'); 
+    game.load.image('enemy', 'assets/images/enemy.png');
+    game.load.image('bullet', 'assets/images/bullet.png');
+}
+
+function create()
+{
+    background = game.add.sprite(0, 0, 'background');
+    background.height = HEIGHT;
+    background.width = WIDTH;
+
+    //game.physics.arcade.gravity.y = 100;
+    
+    platforms = createPlatforms();
+
+    player = createPlayer();
+}
+
+function update()
+{
+
+}
+
+function createPlatforms()
+{
+    // create group
+    platforms = game.add.group();
+    // enable physics on the body
+    platforms.enableBody = true;
+
+    // add platforms
+    for (i = 0; i < platformData.length; i++)
+    {
+        p = platformData[i];
+        platform = platforms.create(p.x, p.y, 'platform');
+        platform.width = p.width
+        platform.body.immovable = true;
+        //platform.body.allowGravity = false;
+    }
+
+    return platforms;
+}
+
+function createPlayer()
+{
+    player = game.add.sprite(0, 0, 'player');
+    game.physics.arcade.enable(player);
+
+    return player;
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Phaser Tutorials
+
+### Included in this project is the source code for the Game Development with JavaScript and Phaser blog post 
+
+The blog post sections have been split into sub directories with the main and final verion sitting in the foot of the directory.
+
+Here is a list of the blog sections and the correct directory for the source code:
+
+Lets Start: Creating the game object and setting up the world - *create_world_and_sprites*
+Adding sprites: create your player and platforms - *create_world_and_sprites*
+Gravity: falling and stopping - *gravity*
